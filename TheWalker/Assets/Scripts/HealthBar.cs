2d746133abde8886c49a0b7cd10f7a135c64f8d5
Anthony <anthony.scottodaniello@ynov.com>
2021-03-08 20:22:30 +0100

    Add The Walker

---
+++ TheWalker/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    
+    public Slider slider;
+
+    public Gradient gradient;
+    public Image fill;
+    public void SetMaxHealth (int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1F);
+    }
+
+    //Une methode Une action #SOLID
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
---
+++ TheWalker/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public float invincibilityTimeAfterHit = 3f;
+    public float invinsibilityFlashDelay = 0.2f;
+
+    public bool isInvicible = false;
+
+    public SpriteRenderer graphics;
+    public HealthBar healthBar;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetHealth(maxHealth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!isInvicible)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+            isInvicible = true;
+            StartCoroutine(InvicibilityFlash());
+            StartCoroutine(HandleInvicibilityDelay());
+        }
+
+    }
+    public IEnumerator InvicibilityFlash()
+    {
+        while (isInvicible)
+        {
+            graphics.color = new Color(1f, 1f, 1f, 0f);
+            yield return new WaitForSeconds (invinsibilityFlashDelay);
+            graphics.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds (invinsibilityFlashDelay);
+        }
+    }
+    public IEnumerator HandleInvicibilityDelay()
+    { 
+        yield return new WaitForSeconds(invincibilityTimeAfterHit);
+        isInvicible = false;
+    }
+}
